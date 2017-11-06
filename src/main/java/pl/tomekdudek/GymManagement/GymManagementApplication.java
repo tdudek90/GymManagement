a2bd9640bd9c74ca3670131bbead6a8c8bd33c45
Tomek Dudek <ttomekk0@gmail.com>
2017-11-06 15:37:32 +0100

    Initial commit

---
+++ src/main/java/pl/tomekdudek/GymManagement/GymManagementApplication.java
@@ -0,0 +1,12 @@
+package pl.tomekdudek.GymManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymManagementApplication.class, args);
+	}
+}
