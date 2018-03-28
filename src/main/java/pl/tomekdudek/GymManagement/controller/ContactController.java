60dc6e0b731a5bc8cc3b0a690b2821812c7d812b
Tomek Dudek <ttomekk0@gmail.com>
2018-03-28 19:26:24 +0200

    added validation in contact form

--- src/main/java/pl/tomekdudek/GymManagement/controller/ContactController.java
+++ src/main/java/pl/tomekdudek/GymManagement/controller/ContactController.java
@@ -3,10 +3,14 @@ package pl.tomekdudek.GymManagement.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.tomekdudek.GymManagement.model.form.ContactForm;
 import pl.tomekdudek.GymManagement.service.MailService;
+
+import javax.validation.Valid;
 
 @Controller
 public class ContactController {
@@ -21,10 +25,12 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/contact", method = RequestMethod.POST)
-    public String contact(ContactForm contactForm, Model model) {
-        model.addAttribute("mailObject", new ContactForm());
-
-        mailService.sendEmail("ttomekk0@gmail.com", new ContactForm(contactForm.getName(), contactForm.getEmail(), contactForm.getPhoneNumber(), contactForm.getMessage()));
-        return "contactSuccess";
+    public String contact(@ModelAttribute("mailObject") @Valid ContactForm contactForm, BindingResult result) {
+        if (result.hasErrors()) {
+            return "contact";
+        } else {
+            mailService.sendEmail("ttomekk0@gmail.com", new ContactForm(contactForm.getName(), contactForm.getEmail(), contactForm.getPhoneNumber(), contactForm.getMessage()));
+            return "contactSuccess";
+        }
     }
 }
--- src/main/java/pl/tomekdudek/GymManagement/model/form/ContactForm.java
+++ src/main/java/pl/tomekdudek/GymManagement/model/form/ContactForm.java
@@ -1,10 +1,17 @@
 package pl.tomekdudek.GymManagement.model.form;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class ContactForm {
 
+    @NotEmpty
     private String name;
+    @Email
     private String email;
+    @NotEmpty
     private String phoneNumber;
+    @NotEmpty
     private String message;
 
     public ContactForm() {
