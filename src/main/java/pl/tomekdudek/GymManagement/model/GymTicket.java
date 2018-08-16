b014eaab8631f63f22ceba6fb32453e2b7e414d4
Tomek Dudek <ttomekk0@gmail.com>
2018-08-16 16:38:08 +0200

    GymTicket class update

--- src/main/java/pl/tomekdudek/GymManagement/model/GymTicket.java
+++ src/main/java/pl/tomekdudek/GymManagement/model/GymTicket.java
@@ -1,8 +1,7 @@
 package pl.tomekdudek.GymManagement.model;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.List;
+import java.sql.Date;
 
 @Entity
 public class GymTicket {
@@ -11,11 +10,60 @@ public class GymTicket {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "gymticket_id")
     private int id;
+    @Column(name = "user_id")
+    private int user_id;
+    private Date dateOfPurchase;
+    private Date expirationDate;
 
-    private LocalDate dateOfPurchase;
-    private LocalDate expirationDate;
+    public GymTicket() {
+    }
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(name = "user_gymticket", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "gymticket_id"))
-    private List<GymTicket> gymTickets;
+    public GymTicket(int id, int user_id, Date dateOfPurchase, Date expirationDate) {
+        this.id = id;
+        this.user_id = user_id;
+        this.dateOfPurchase = dateOfPurchase;
+        this.expirationDate = expirationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(Date dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "GymTicket{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", dateOfPurchase=" + dateOfPurchase +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
 }
